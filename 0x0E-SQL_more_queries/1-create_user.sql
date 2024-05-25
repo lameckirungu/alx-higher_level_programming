425d86d52dc5c03cb54145b4450a574909dd3b8d
Lameck Irungu <mugolameck@gmail.com>
2024-05-25 06:18:26 -0700

    Create MySQL user 'user_0d_1' with temporary password and grant all privileges (localhost only)

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- Creates a MySQL server User `user_0d_1` with the password set to `user_0d_1`
+DROP USER IF EXISTS 'user_0d_1@localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1@localhost';
+FLUSH PRIVILEGES;
