13678368695bc4e8ad2e8e35797ec08c5f683ff0
Lameck Irungu <mugolameck@gmail.com>
2024-05-26 02:58:10 -0700

    Initial setup: Created database 'hbtn_0d_2' and user 'user_0d_2' with SELECT privileges

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- A script that creates the database 'hbtn_0d_2' and the 'user user_0d_2'.
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
