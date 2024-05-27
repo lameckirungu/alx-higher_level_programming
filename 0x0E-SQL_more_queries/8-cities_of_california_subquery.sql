21ee1b816b087cf51b644351b2082b30315cb144
Lameck Irungu <mugolameck@gmail.com>
2024-05-26 23:02:40 -0700

    List cities (ascending order) from California (subquery approach) in database passed as argument

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- script lists all the cities of california from the database 'hbtn_0d_usa'
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (
 	SELECT id FROM states
 	WHERE name = 'California'
