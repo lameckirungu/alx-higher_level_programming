7b25cade85692df6361e864d32d36dbfa296f5c1
Lameck Irungu <mugolameck@gmail.com>
2024-05-27 01:50:06 -0700

    Updated 9-cities_by_state_join.sql to remove redundant code

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 
 SELECT c.id, c.name, s.name
 FROM hbtn_0d_usa.cities AS c
-INNER JOIN hbtn_0d_usa.states AS s ON c.state_id = s.id
+JOIN hbtn_0d_usa.states AS s ON c.state_id = s.id
 ORDER BY c.id ASC
